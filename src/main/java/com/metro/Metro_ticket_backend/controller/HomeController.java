9c15c3a01ccbfa35fde4aaab0badc255964af095
muskan <muskanmulla799@gmail.com>
2026-03-15 15:05:26 +0530

    updated application

---
+++ src/main/java/com/metro/Metro_ticket_backend/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.metro.Metro_ticket_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Metro Ticket Backend is Running Successfully!";
+    }
+}
